fb19d48c95e977baa478e8fd6dd6857590aaa2d2
angelaramiz <angelaramiz22@gmail.com>
2025-06-27 04:44:54 -0600

    refactor: Eliminar mapeo estático de ubicaciones en la función de guardar inventario

--- js/product-operations.js
+++ js/product-operations.js
@@ -609,17 +609,6 @@ export function limpiarFormularioInventario() {
     }
 }
 
-// funcion para guardar productos en la base de datos para inventariar
-// Mapeo de nombres de ubicaciones a sus IDs
-//remmplazar por consulta dinamica a la base de dts
-const ubicaciones = {
-    'cámara fría': '10000000-0000-0000-0000-000000000001',
-    'congelador interior': '10000000-0000-0000-0000-000000000002',
-    'bunker': '10000000-0000-0000-0000-000000000003',
-    'rishin': '10000000-0000-0000-0000-000000000004',
-    'piso': '10000000-0000-0000-0000-000000000005'
-};
-
 // Función para guardar productos en la base de datos para inventariar
 export async function guardarInventario() {
     const codigo = document.getElementById("codigoProductoInventario")?.value;
